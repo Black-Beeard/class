cf3956797ac78dec0e8f87485aae3f235095f74d
Rafi Mahafid <rafimahafid@gmail.com>
2021-09-03 11:46:39 +0600

    Fixed By Rafi

--- classes/bangla.php
+++ classes/bangla.php
@@ -1,12 +1,11 @@
 <?php
 
-class Dhaka{
-    
-    public function city(){
-        echo "Base Class constant and class name-> ".__CLASS__."<br>";
-        echo "Base Class function and class name-> ".get_class($this)."<br>";
+class Dhaka
+{
+
+    public function city()
+    {
+        echo "Base Class constant and class name-> " . __CLASS__ . "<br>";
+        echo "Base Class function and class name-> " . get_class($this) . "<br>";
     }
 }
-
-
-?>
